61268769754274aa240cc57152999a3b75e76e98
siliang cao <siliang.cao@grabtaxi.com>
2016-11-20 11:13:36 +0800

    use future copy

--- src/main.rs
+++ src/main.rs
@@ -38,37 +38,32 @@
 //! throwing connections at it yourself, and there should be plenty of comments
 //! below to help walk you through the implementation as well!
 
-#[macro_use]
-extern crate log;
+#[macro_use] extern crate log;
 extern crate env_logger;
-#[macro_use]
-extern crate futures;
-#[macro_use]
-extern crate tokio_core;
+#[macro_use] extern crate futures;
+#[macro_use] extern crate tokio_core;
 extern crate futures_cpupool;
 
-use std::cell::RefCell;
-use std::rc::Rc;
 use std::env;
-use std::io::{self, Read, Write};
+use std::io;
 use std::net::{SocketAddr, Ipv4Addr, Ipv6Addr, SocketAddrV4, SocketAddrV6};
-use std::net::Shutdown;
 use std::str;
 use std::time::Duration;
 
-use futures::{Future, Poll, Async};
+use futures::{Future};
 use futures::stream::Stream;
 use futures_cpupool::CpuPool;
 use tokio_core::reactor::{Core, Handle, Timeout};
 use tokio_core::net::{TcpStream, TcpListener};
 use tokio_core::io::{read_exact, write_all, Window};
+use tokio_core::io::{copy, Io};
 
 fn main() {
     drop(env_logger::init());
 
     // Take the first command line argument as an address to listen on, or fall
     // back to just some localhost default.
-    let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
+    let addr = env::args().nth(1).unwrap_or("127.0.0.1:18080".to_string());
     let addr = addr.parse::<SocketAddr>().unwrap();
 
     // Initialize the various data structures we're going to use in our server.
@@ -77,7 +72,6 @@ fn main() {
     // into, and finally binding the TCP listener itself.
     let mut lp = Core::new().unwrap();
     let pool = CpuPool::new(4);
-    let buffer = Rc::new(RefCell::new(vec![0; 64 * 1024]));
     let handle = lp.handle();
     let listener = TcpListener::bind(&addr, &handle).unwrap();
 
@@ -87,7 +81,6 @@ fn main() {
     println!("Listening for socks5 proxy connections on {}", addr);
     let clients = listener.incoming().map(move |(socket, addr)| {
         (Client {
-            buffer: buffer.clone(),
             pool: pool.clone(),
             handle: handle.clone(),
         }.serve(socket), addr)
@@ -117,7 +110,6 @@ fn main() {
 // Data used to when processing a client to perform various operations over its
 // lifetime.
 struct Client {
-    buffer: Rc<RefCell<Vec<u8>>>,
     pool: CpuPool,
     handle: Handle,
 }
@@ -139,7 +131,7 @@ impl Client {
     /// Once we've got the version byte, we then delegate to the below
     /// `serve_vX` methods depending on which version we found.
     fn serve(self, conn: TcpStream)
-              -> Box<Future<Item=(u64, u64), Error=io::Error>> {
+             -> Box<Future<Item = (u64, u64), Error = io::Error>> {
         Box::new(read_exact(conn, [0u8]).and_then(|(conn, buf)| {
             match buf[0] {
                 v5::VERSION => self.serve_v5(conn),
@@ -156,7 +148,7 @@ impl Client {
 
     /// Current SOCKSv4 is not implemented, but v5 below has more fun details!
     fn serve_v4(self, _conn: TcpStream)
-                -> Box<Future<Item=(u64, u64), Error=io::Error>> {
+                -> Box<Future<Item = (u64, u64), Error = io::Error>> {
         futures::failed(other("unimplemented")).boxed()
     }
 
@@ -172,7 +164,7 @@ impl Client {
     /// Essentially, the `.boxed()` annotations here improve compile times, but
     /// are otherwise not necessary.
     fn serve_v5(self, conn: TcpStream)
-                -> Box<Future<Item=(u64, u64), Error=io::Error>> {
+                -> Box<Future<Item = (u64, u64), Error = io::Error>> {
         // First part of the SOCKSv5 protocol is to negotiate a number of
         // "methods". These methods can typically be used for various kinds of
         // proxy authentication and such, but for this server we only implement
@@ -462,135 +454,19 @@ impl Client {
         // create two independent `Transfer` futures representing each half of
         // the connection. These two futures are `join`ed together to represent
         // the proxy operation happening.
-        let buffer = self.buffer.clone();
         mybox(pair.and_then(|(c1, c2)| {
-            let c1 = Rc::new(c1);
-            let c2 = Rc::new(c2);
+            let (client_reader, client_writer) = c1.split();
+            let (server_reader, server_writer) = c2.split();
 
-            let half1 = Transfer::new(c1.clone(), c2.clone(), buffer.clone());
-            let half2 = Transfer::new(c2, c1, buffer);
-            half1.join(half2)
+            let upload = copy(server_reader, client_writer);
+            let download = copy(client_reader, server_writer);
+            upload.join(download)
         }))
     }
 }
 
-fn mybox<F: Future + 'static>(f: F) -> Box<Future<Item=F::Item, Error=F::Error>> {
+fn mybox<F: Future + 'static>(f: F) -> Box<Future<Item = F::Item, Error = F::Error>> {
     Box::new(f)
-}
-
-/// A future representing reading all data from one side of a proxy connection
-/// and writing it to another.
-///
-/// This future, unlike the handshake performed above, is implemented via a
-/// custom implementation of the `Future` trait rather than with combinators.
-/// This is intended to show off how the combinators are not all that can be
-/// done with futures, but rather more custom (or optimized) implementations can
-/// be implemented with just a trait impl!
-struct Transfer {
-    // The two I/O objects we'll be reading.
-    reader: Rc<TcpStream>,
-    writer: Rc<TcpStream>,
-
-    // The shared global buffer that all connections on our server are using.
-    buf: Rc<RefCell<Vec<u8>>>,
-
-    // The number of bytes we've written so far.
-    amt: u64,
-}
-
-impl Transfer {
-    fn new(reader: Rc<TcpStream>,
-           writer: Rc<TcpStream>,
-           buffer: Rc<RefCell<Vec<u8>>>) -> Transfer {
-        Transfer {
-            reader: reader,
-            writer: writer,
-            buf: buffer,
-            amt: 0,
-        }
-    }
-}
-
-// Here we implement the `Future` trait for `Transfer` directly. This does not
-// use any combinators, and shows how you might implement it in custom
-// situations if needed.
-impl Future for Transfer {
-    // Our future resolves to the number of bytes transferred, or an I/O error
-    // that happens during the connection, if any.
-    type Item = u64;
-    type Error = io::Error;
-
-    /// Attempts to drive this future to completion, checking if it's ready to
-    /// be completed.
-    ///
-    /// This method is the core foundation of completing a future over time. It
-    /// is intended to never block and return "quickly" to ensure that it
-    /// doesn't block the event loop.
-    ///
-    /// Completion for our `Transfer` future is defined when one side hits EOF
-    /// and we've written all remaining data to the other side of the
-    /// connection. The behavior of `Future::poll` is in general not specified
-    /// after a future resolves (e.g. in this case returns an error or how many
-    /// bytes were transferred), so we don't need to maintain state beyond that
-    /// point.
-    fn poll(&mut self) -> Poll<u64, io::Error> {
-        let mut buffer = self.buf.borrow_mut();
-
-        // Here we loop over the two TCP halves, reading all data from one
-        // connection and writing it to another. The crucial performance aspect
-        // of this server, however, is that we wait until both the read half and
-        // the write half are ready on the connection, allowing the buffer to
-        // only be temporarily used in a small window for all connections.
-        loop {
-            let read_ready = self.reader.poll_read().is_ready();
-            let write_ready = self.writer.poll_write().is_ready();
-            if !read_ready || !write_ready {
-                return Ok(Async::NotReady)
-            }
-
-            // TODO: This exact logic for reading/writing amounts may need an
-            //       update
-            //
-            // Right now the `buffer` is actually pretty big, 64k, and it could
-            // be the case that one end of the connection can far outpace
-            // another. For example we may be able to always read 64k from the
-            // read half but only be able to write 5k to the client. This is a
-            // pretty bad situation because we've got data in a buffer that's
-            // intended to be ephemeral!
-            //
-            // Ideally here we'd actually adapt the rate of reads to match the
-            // rate of writes. That is, we'd prefer to have some form of
-            // adaptive algorithm which keeps track of how many bytes are
-            // written and match the read rate to the write rate. It's possible
-            // for connections to have an even smaller (and optional) buffer on
-            // the side representing the "too much data they read" if that
-            // happens, and then the next call to `read` could compensate by not
-            // reading so much again.
-            //
-            // In any case, though, this is easily implementable in terms of
-            // adding fields to `Transfer` and is complicated enough to
-            // otherwise detract from the example in question here. As a result,
-            // we simply read into the global buffer and then assert that we
-            // write out exactly the same amount.
-            //
-            // This means that we may trip the assert below, but it should be
-            // relatively easily fixable with the strategy above!
-
-            let n = try_nb!((&*self.reader).read(&mut buffer));
-            if n == 0 {
-                try!(self.writer.shutdown(Shutdown::Write));
-                return Ok(self.amt.into())
-            }
-            self.amt += n as u64;
-
-            // Unlike above, we don't handle `WouldBlock` specially, because
-            // that would play into the logic mentioned above (tracking read
-            // rates and write rates), so we just ferry along that error for
-            // now.
-            let m = try!((&*self.writer).write(&buffer[..n]));
-            assert_eq!(n, m);
-        }
-    }
 }
 
 fn other(desc: &str) -> io::Error {
